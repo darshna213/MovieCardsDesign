81931f368b8756ae915511bbcdad07fb6c951df5
darshna213 <93382999+darshna213@users.noreply.github.com>
2022-08-21 23:58:19 +0530

    card component design

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Card';
+ReactDOM.render(
+  <>
+    <Card movieName="herry potter"
+
+    />
+  </>,
+  document.getElementById("root")
+);
