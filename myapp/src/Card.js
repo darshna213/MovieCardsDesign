31ee24a0ed83483ae08bbddc5cbafd6d69bcc0f7
darshna213 <93382999+darshna213@users.noreply.github.com>
2022-08-23 09:29:25 +0530

    using props and design two movie cards

--- myapp/src/Card.js
+++ myapp/src/Card.js
@@ -6,22 +6,21 @@ function Card(props) {
         <div className="cards">
             <div className="movie-card-container">
                 <div className="image-container">
-                    <img src="https://m.media-amazon.com/images/M/MV5BMTQ2OTE1Mjk0N15BMl5BanBnXkFtZTcwODE3MDAwNA@@._V1_FMjpg_UX1000_.jpg" alt="image1" />
+                    <img src={props.movieImageUrl} alt="movieposter" />
                 </div>
                 <div className="movie-detail-container">
                     <div className="movie-name">{props.movieName}</div>
-                    <div className="movie-name-header-detail">and the deathly hallows part 1</div>
+                    <div className="movie-name-header-detail">{props.headerDetail}</div>
                     <div className="movie-details">
-                        <div className="movie-release-year">2010</div>
-                        <div className="movie-time">146min</div>
-                        <div className="movie-type">Adventure, Fantasy, Family</div>
+                        <div className="movie-release-year">{props.releaseYear}</div>
+
+                        <div className="movie-time">{props.movieTime}</div>
+                        <div className="movie-type">{props.movieType}</div>
                     </div>
-                    <div className="movie-description">
-                        As Harry races against time and evil to destroy the Horcruxes, he uncovers the existenceof three most powerful objects in the wizarding world: the Deathly Hallows.
-                    </div>
+                    <div className="movie-description">{props.movieDescription}</div>
                     <div className="rating">
                         <div className="rating-title">Rating:</div>
-                        <div className="rating-count">13</div>
+                        <div className="rating-count">{props.rating}</div>
                     </div>
                     <div className="buttons">
                         <div className="watch-trailer-button">Watch Trailer</div>
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,11 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
+
 ReactDOM.render(
   <>
     <Card movieName="herry potter"
-
+    movieImageUrl="https://m.media-amazon.com/images/M/MV5BMTQ2OTE1Mjk0N15BMl5BanBnXkFtZTcwODE3MDAwNA@@._V1_FMjpg_UX1000_.jpg"
+      headerDetail="and the deathly hallows part 1"
+      releaseYear="2010"
+      movieTime="146min"
+      movieType="Adventure, Fantasy, Family"
+      movieDescription="As Harry races against time and evil to destroy the Horcruxes, he uncovers the existenceof three most powerful objects in the wizarding world: the Deathly Hallows."
+      rating="13"
     />
+
+    <Card movieName="perfume"
+    movieImageUrl="https://perfumesociety.org/wp-content/uploads/2020/12/Fragrant-Film-Club-Perfume-movie-683x1024.jpg"
+      headerDetail="the story of a murderer"
+      releaseYear="2006"
+      movieTime="147min"
+      movieType="Thriller/Drama"
+      movieDescription="Set in 18th century Paris, the story of a man with an extraordinarily acute sense of smell takes a dark turn when his quest to create the ultimate fragrance leads to murder."
+      rating="20"
+    />
+    
   </>,
   document.getElementById("root")
 );
